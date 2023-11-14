dff55b452cd82e7836cde3623e6fd5ffc7e9c039
dakotas17 <dakotas17@nycstudents.net>
2023-11-14 09:22:00 -0500

    wae

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,28 @@
-const Menu = ["za", "mnms"];
-const Name = "denizee";
-export {Menu, Name};
+const opc = [
+    {
+        name: "luffy",
+        gender: "man",
+        crew: "straw hats",
+        devilfruit: "yes"
+    },{
+        name: "zoro",
+        gender: "man",
+        crew: "straw hats",
+        devilfruit: "no"
+    },{
+        name:"sanji",
+        gender: "man",
+        crew: "straw hats",
+        devilfruit: "no"
+    },{
+        name: "nami",
+        gender: "woman",
+        crew: "straw hat",
+        devilfruit: "no"
+    },{
+        name: "robin",
+        gender: "woman",
+        crew: "straw hats",
+        devilfruit: "yes"
+    }
+];
