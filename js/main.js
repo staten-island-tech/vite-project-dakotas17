b179b05d6a65145997a67cca61fe920ea2e0c9eb
dakotas17 <dakotas17@nycstudents.net>
2023-11-27 09:20:55 -0500

    8tyv;y

--- js/main.js
+++ js/main.js
@@ -13,37 +13,13 @@ function uncheck() {
 }
 
 
-function section(){
-    opc.container.insertAdjacentHTML(
-    `<div class="section">
-    <h1> ${opc.name.value}</h1>
-    <h2> ${opc.gender.value}</h2>
-    <h3> ${opc.crew.value}</h3>
-</div>`)}
-
-opc.forEach(opc => {
-    var div = document.querySelector(opc)
-    div.innerHTML = div.innerHTML + `
-    <div class="opc-card">
-    <div class="badge">Hot</div>
-    <div class="product-tumb">
-    <img src="" alt="" />
-    </div>
-    <div class="product-details">
-    <span class="product-catagory">dog1,dog2</span>
-    <h4><a href="">${opc.name}</a></h4>
-    <p>
-    ${opc.description}</p>
-    <div class="product-bottom-details">
-    <div class="product-price">
-    <small>$96.00</small>${opc.age}
-    </div>
-    <button>buy now</button>
-    <div class="product-links">
-    <a><i class="fa fa-heart"></i></a>
-    <a><i class="fa fa-shopping-cart"></i></a>
-    </div>
-    </div>
-    </div>
-    </div>`
+function Insert(arr){
+arr.forEach((opc) => {
+    document.querySelector(".container").insertAdjacentHTML("afterbegin",
+    `<div class ="card">
+    <h1 class= "card-title">${opc.name}</h1>
+    <h2 class= "
+    
+    `)
 })
+}
