de5844bbb06eac1661da02673639c5adf6b0a709
dakotas17 <dakotas17@nycstudents.net>
2024-01-15 22:47:24 -0500

    uytfi

--- vite-project/menu.js
+++ vite-project/menu.js
@@ -1,30 +1,30 @@
 const opc = [
     {
         name: "luffy",
-        type: "Man",
+        type: ["Man", "All"],
         age: "19",
         img: "https://staticg.sportskeeda.com/editor/2023/07/35206-16893440521036-1920.jpg?w=840",
     },{
         name: "zoro",
-        type: "Man",
+        type: ["Man", "All"],
         age: "21",
         img: "https://i.pinimg.com/originals/0a/ba/26/0aba26f8a35fc351c7f33cbcac66b467.png",
         
     },{
         name:"sanji",
-        type: "Man",
+        type: ["Man", "All"],
         age: "21",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLkVlU0PnGLNjqKLRZGqA6Iy4_djEtLy66dw&usqp=CAU",
        
     },{
         name: "nami",
-        type: "Woman",
+        type: ["Woman", "All"],
         age: "20",
         img: "https://i.scdn.co/image/ab67616d0000b2738c5b943cb9cfbc924e4bf9cc",
         
     },{
         name: "robin",
-        type: "Woman",
+        type: ["Woman", "All"],
         age: "30",
         img: "https://i.pinimg.com/236x/d4/b8/0f/d4b80f3235faa59fa00390c57412ee51.jpg",
         
