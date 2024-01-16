c9d9c44baba9d5043539326c730387eedfd13ab9
dakotas17 <dakotas17@nycstudents.net>
2024-01-15 22:20:31 -0500

    awdew

--- vite-project/main.js
+++ vite-project/main.js
@@ -29,6 +29,10 @@ if (document.body.classList.contains("lightmode")){
 }
 });
 
+function remove(){
+  const clear = document.getElementById("container");
+  clear.innerHTML = ""
+};
 
 let buttons= document.querySelectorAll(".btn")
 
